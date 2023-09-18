c7f573ddf796c887fc99751056e6e9d22c71e4c0
Dheeraj Sharma <dheerajsksd40@gmail.com>
2023-09-18 11:33:40 +0530

    spinner added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import './Home.css';
-import LeftSidebar from '../../components/LeftSidebar/LeftSidebar';
-import ShowVideoGrid from '../../components/ShowVideoGrid/ShowVideoGrid';
-import { useSelector } from 'react-redux';
+import React from "react";
+import "./Home.css";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import ShowVideoGrid from "../../components/ShowVideoGrid/ShowVideoGrid";
+import LoadingSpin from "react-loading-spin";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-
-  const vids = useSelector(state => state.videoReducer)?.data?.filter(q => q).reverse();
+  const vids = useSelector((state) => state.videoReducer)
+    ?.data?.filter((q) => q)
+    .reverse();
 
   const NavList = [
     "All",
@@ -17,26 +19,30 @@ const Home = () => {
     "Science",
     "Animation",
     "Gaming",
-    "Comedy"
+    "Comedy",
   ];
 
   return (
-    <div className='container_Pages_App'>
+    <div className="container_Pages_App">
       <LeftSidebar />
-      <div className='container2_Pages_App'>
+      <div className="container2_Pages_App">
         <div className="navigation_Home">
-        {
-          NavList.map(item => (
+          {NavList.map((item) => (
             <p key={item} className="btn_nav_home">
               {item}
             </p>
-          ))
-        }
+          ))}
         </div>
-        <ShowVideoGrid vids={vids}/>
+        {!vids && (
+          <div className="videoLoader">
+            <LoadingSpin primaryColor="#3ea6ff" secondaryColor="transparent" />
+            <span>Loading Videos...</span>
+          </div>
+        )}
+        {vids && <ShowVideoGrid vids={vids} />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
